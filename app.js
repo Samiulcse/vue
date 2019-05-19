ab86948671305358eb770a7cd8e5897dff086ddc
Hasan <samiulcse@yahoo.com>
2019-05-19 12:43:27 +0600

    basic component created

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+
+Vue.component('card', {
+
+    props: [
+        'title', 'content'
+    ],
+    
+    template: `<div class="card  mt-5">
+    <div class="card-body">
+        <h3 class="card-title">
+           {{ title }}
+        </h3>
+        <div class="card-text">
+            {{ content }}
+        </div>
+    </div>
+</div>`
+  })
+
+  
+
 new Vue({
     el: '#app',
     data: {
