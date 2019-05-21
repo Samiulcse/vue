e16605d61f6dd5532f136faca465631d73154fb7
Hasan <samiulcse@yahoo.com>
2019-05-21 16:10:39 +0600

    Build-a-shopping-cart-L4-Create-the-product-component done

--- vue-practice-shop/src/App.vue
+++ vue-practice-shop/src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-7"></div>
+      <div class="col-md-7">
+        <div class="row">
+          <div class="col-md-6" v-for="product in products" v-bind:key="product.id">
+            <product></product>
+          </div>
+        </div>
+      </div>
       <div class="col-md-5"></div>
     </div>
   </div>
 </template>
 
 <script>
+
+import products from '@/products.json';
+import Product from '@/components/Product.vue';
+
 export default {
   name: "app",
+
+  components:{
+    Product
+  },
+
+  data(){
+    return {
+      products
+    }
+  }
 };
 </script>
 
---
+++ vue-practice-shop/src/components/Product.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="card my-5">
+        <div class="card-body">
+            <h4 class="text-center card-title">Product Name</h4>
+            <p class="text-center text-muted card-text display-4">$275.5</p>
+            <button class="btn btn-primary btn-sm form-control">Add To Cart</button>
+        </div>
+    </div>
+</template>
